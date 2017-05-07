77ea5cf0c66a23916e909e8f531a9f987330c153
root <namibj.der.echte+ghcontact@gmail.com>
2017-05-07 20:23:59 +0200

    Added the remaining file insexing logic/boilerplate.

--- main.c
+++ main.c
@@ -10,6 +10,8 @@
 /*//Scratchpads
 char* spad1 = malloc(sizeof(char)* 1<<20);
 char* spad2 = malloc(sizeof(char)* 1<<20);*/
+sqlite3_stmt* stmt = NULL;
+sqlite3* db = NULL;
 sqlite3* open_db(char mode){
 	/*FILE* db = malloc(sizeof(FILE));
 	bool root = false;
@@ -38,14 +40,19 @@ int add_callback(comst char* fpath, const struct stat *sb, int typeflag, struct 
 	if(effective_filelength > SQLITE_MAX_LENGTH - 1) effective_filelength = SQLITE_MAX_LENGTH - 1;
 	int fd = open(fpath, O_RDONLY | O_NOATIME);
 	char * file_content = (char *) mmap(NULL, effective_filelength, PROT_READ, 0, fd, 0);
-
-	sqlite3_bind_text(stmt, 1, file_content, effective_filelength, SQLITE_STATIC);
-
+	sqlite3_reset(stmt);
+	sqlite3_bind_text(stmt, 1, fpath, -1, SQLITE_STATIC);
+	sqlite3_bind_text(stmt, 2, file_content, effective_filelength, SQLITE_STATIC);
+	while(sqlite3_step(stmt) != SQLITE_DONE);
 	munmap((void *) file_content, effective_filelength);
+	close(fd);
 }
 void add(char *in) {
+	db = open_db('');
 	sqlite3_exec(db, "BEGIN;", NULL, NULL, NULL);
+	sqlite3_prepare(db, "INSERT INTO files (path, content) VALUES (?, ?);", -1, &stmt, NULL)z;
 	nftw(in, &add_callback, 10, 0);// traverse folder, and use prepared statement to add all of the folder in one transaction. TODO: insert debug macro for immediate transactions.
+	sqlite3_finalize(stmt);
 	sqlite3_exec(db, "COMMIT;", NULL, NULL, NULL);
 }
  
